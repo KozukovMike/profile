88ccc37c080b111e7864564845649e54fc756db9
M.Kozyukov <kozukovmisa@gmail.com>
2024-01-24 12:14:33 +0300

    "create an autontification with fastapi users"

---
+++ autintification/database.py
@@ -0,0 +1,37 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declarative_base
+from sqlalchemy import String, Boolean, Column, Integer, TIMESTAMP, Table, JSON, ForeignKey
+from config import DB_USER, DB_HOST, DB_NAME, DB_PORT, DB_PASS
+from models.models import role
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+Base: DeclarativeBase = declarative_base()
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column("id", Integer, primary_key=True)
+    username = Column("username", String, nullable=False)
+    role_id = Column("role_id", Integer, ForeignKey(role.c.id))
+    email = Column("email", String, nullable=False)
+    hashed_password = Column("hashed_password", String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+    is_verified = Column(Boolean, default=False, nullable=False)
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from urllib import request
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from fastapi_users import fastapi_users, FastAPIUsers
 
+from autintification.database import User
+from autintification.main_auth import auth_backend
+from autintification.manager import get_user_manager
+from autintification.schemas import UserRead, UserCreate
 
 templates = Jinja2Templates(directory='static/tamplates')
 app = FastAPI()
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
